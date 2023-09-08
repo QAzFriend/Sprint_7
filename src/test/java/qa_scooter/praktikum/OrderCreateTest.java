259458c3c8fe0026f8098002237db564766a4bcf
QAzFriend <129176730+QAzFriend@users.noreply.github.com>
2023-09-08 16:27:27 +0400

    Second makingChanges

--- src/test/java/qa_scooter/praktikum/OrderCreateTest.java
+++ src/test/java/qa_scooter/praktikum/OrderCreateTest.java
@@ -1,5 +1,6 @@
 package qa_scooter.praktikum;
 
+import io.qameta.allure.Step;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.junit.Before;
@@ -8,9 +9,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import qa_scooter.praktikum.constants.Url;
 import qa_scooter.praktikum.order.Order;
-
 import java.util.List;
-
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static qa_scooter.praktikum.utils.Utils.randomInt;
@@ -18,38 +17,44 @@ import static qa_scooter.praktikum.utils.Utils.randomString;
 @RunWith(Parameterized.class)
 public class OrderCreateTest {
 
-        private final String name;
-        private final String surname;
-        private final String address;
-        private final String metroStation;
-        private final String phone;
-        private final int rentTime;
-        private final String deliveryDate;
-        private final String comments;
+        private  String name;
+        private  String surname;
+        private  String address;
+        private  String metroStation;
+        private  String phone;
+        private  int rentTime;
+        private  String deliveryDate;
+        private  String comments;
         private List<String> color;
 
 
-        public OrderCreateTest(String name, String surname, String address, String metroStation,String phone,int rentTime, String deliveryDate, String comments, List<String> color) {
-            this.name = name;
-            this.surname = surname;
-            this.address = address;
-            this.metroStation = metroStation;
-            this.phone = phone;
-            this.rentTime = rentTime;
-            this.deliveryDate = deliveryDate;
-            this.comments = comments;
-            this.color = color;
-        }
-//Решил оставить с параметром только цвет
+      public OrderCreateTest(List<String> color) {this.color = color;}
+
+//да понял, немного затупил при прочтении ревью))
         @Parameterized.Parameters
         public static Object[][] inputUser() {
             return new Object[][]{
-                    {randomString(8), randomString(8), randomString(20), randomString(20), "79999999999", randomInt(), "2023-06-01", randomString(25),List.of("BLACK", "GRAY")},
+                    {List.of()},
+                    {List.of("BLACK")},
+                    {List.of("BLACK", "GREY")},
+                    {List.of("GREY")},
             };
         }
     @Before
     public void setUp() {
         RestAssured.baseURI = Url.BASE_URI;
+    }
+    @Before
+    @Step("Подготовка тестовых данных")
+    public void prepareTest(){
+        this.name = randomString(8);
+        this.surname = randomString(8);
+        this.address = randomString(20);
+        this.metroStation = randomString(20);
+        this.phone = "79508111579";
+        this.rentTime = randomInt();
+        this.deliveryDate = "2023-06-01";
+        this.comments = randomString(25);
     }
 
     @Test
